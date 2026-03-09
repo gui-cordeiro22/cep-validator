20f0dfd82c2f87fe28a33f0c3f6e99ed1e304807
Guilherme Cordeiro <guilhermecordeiro43@gmail.com>
2026-03-09 17:47:16 -0300

    feat(assets): adding some icons

--- src/components/sections/header/header.component.tsx
+++ src/components/sections/header/header.component.tsx
@@ -4,11 +4,22 @@ import { FunctionComponent } from "react";
 // Styles
 import { Container } from "./header.styles";
 
+// Assets
+import pinMap from "@/assets/svg/map-pin-check.svg";
+
 // Types
 import { HeaderProps } from "./header.types";
 
 export const Header: FunctionComponent<HeaderProps> = ({
     brandTitleElement,
 }) => {
-    return <Container>{brandTitleElement}</Container>;
+    return (
+        <Container>
+            <div>
+                <img src={pinMap} alt="" />
+
+                {brandTitleElement}
+            </div>
+        </Container>
+    );
 };
--- src/components/sections/header/header.styles.ts
+++ src/components/sections/header/header.styles.ts
@@ -7,9 +7,21 @@ import { pageGutter } from "@/styles/gutter";
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
-    align-items: center;
+    align-items: flex-start;
     justify-content: center;
     width: 100%;
 
     ${pageGutter}
+
+    div {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: flex-start;
+        gap: 10px;
+
+        img {
+            width: 32px;
+        }
+    }
 `;
